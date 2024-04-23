bb099b681c66b07156543f397cc94c821d4969d7
Danilo <danilo-bc@users.noreply.github.com>
2024-04-23 17:19:16 -0300

    Added Numba for comparison

--- 04_libmath_sin/main.py
+++ 04_libmath_sin/main.py
@@ -6,8 +6,17 @@ from math import sin
 n_iter = 5000000
 x = 3.14151920
 
+from numba import njit
+@njit
+def f_numba(x):
+    return sin(x * x)
+
 print("Python:")
 print(timeit.Timer("sin(x * x)", globals=globals()).timeit(number=n_iter)/n_iter)
+
+print("Numba:")
+f_numba(x)
+print(timeit.Timer("f_numba(x)", globals=globals()).timeit(number=n_iter)/n_iter)
 
 print("Numpy:")
 print(timeit.Timer("np.sin(x * x)", globals=globals()).timeit(number=n_iter)/n_iter)
