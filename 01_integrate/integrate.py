b40b1edac9da0cad9d279757a18a4a34561dc1f6
Danilo <danilo-bc@users.noreply.github.com>
2024-04-23 09:35:23 -0300

    Added function type to speed up cython version

--- 01_integrate/integrate.py
+++ 01_integrate/integrate.py
@@ -1,9 +1,11 @@
 import cython
 
-def f(x: cython.double):
+@cython.cfunc
+@cython.exceptval(-2, check=True)
+def f(x: cython.double) -> cython.double:
     return x ** 2 - x
 
-def integrate_f(a: cython.double, b: cython.double, N: cython.int):
+def integrate_f(a: cython.double, b: cython.double, N: cython.int) -> cython.double:
     i: cython.int
     s: cython.double
     dx: cython.double
