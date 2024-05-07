67c00c210bb8c1afd4367346ed8521b19379c2b3
Danilo <danilo-bc@users.noreply.github.com>
2024-05-07 11:09:44 -0300

    Modified type hint to Cython type. No noticeable changes with this.

--- 05p2_c_into_python/cpp_operations.pyx
+++ 05p2_c_into_python/cpp_operations.pyx
@@ -1,11 +1,12 @@
 # distutils: language = c++
 # cython: language_level = 3
 from libcpp.vector cimport vector
+import cython
 cdef extern double cpp_convolve(const vector[vector[double] ]& in_a_v, const vector[vector[double] ]& in_b_v, int n_vecs)
 
 ctypedef vector[double] vector_double_t
 
 def cpp_convolve_py(in_a_v: vector[vector_double_t], in_b_v: vector[vector_double_t], n_vecs: int):
-    res: float
+    res: cython.double
     res = cpp_convolve(in_a_v, in_b_v, n_vecs)
     return res
